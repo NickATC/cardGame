4f93a6961822d21d935fd0696443e16795e161f0
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2022-04-01 09:48:53 -0500

    Initial Commit

---
+++ Card_Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card_Game
+//
+//  Created by NICOLAS  TAUTIVA on 1/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
